c40efc0f23462664050444d6699ce721b4e73ded
FERNANDA NALON PIRES SAUAIA <fernanda.npsauaia@senacsp.edu.br>
2026-05-26 16:54:49 -0300

    fix: corrige caminho do arquivo CSS na tela de sucesso

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,7 +82,7 @@ function exibirSucesso(req, res) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Login realizado</title>
-      <link rel="stylesheet" href="/style.css">
+      <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
       <main class="container">
